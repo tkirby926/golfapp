a47a8bd6db0eb6750fe51d0712303a68f5e3496f
Thomas Kirby <tmkirby@umich.edu>
2023-01-26 02:16:44 -0500

    Change

--- src/components/AllMessageConvoComponent.js
+++ src/components/AllMessageConvoComponent.js
@@ -58,39 +58,37 @@ export class AllMessagesComponent extends React.Component {
             width_form_a = "100%";
             width_form_b = "90%";
         }
-        else {
-            return (<div>
-                <div style={{width: '100%', justifyContent: 'center', display: 'flex'}}>
-                        <button hidden={!this.state.under_width} class="button4" style={{float: 'left', background: 'green', padding: '5px', marginRight: '8vw', marginTop: '3vh'}} onClick={(event) => this.changeView(event, true)}>Conversations</button>
-                        <button hidden={!this.state.under_width} class="button4" style={{float: 'left', background: 'green', padding: '5px', marginTop: '3vh'}} onClick={(event) => this.changeView(event, false)}>GolfTribe Courses</button>
-                </div>
-                <div hidden={this.state.under_width && !this.state.message_view} style={{width: width_form_a, float: 'left'}}>
-                    <div style={{border: 'thick solid gray', minHeight: '80vh', borderRadius: '25px'}}>
-                    <h3 style={{textAlign: 'center'}}>Message Conversations</h3>
-                    {this.state.messages_list.map((user, index) => {
-                        var url = '/messages?id=' + this.state.matching_users[index];
-                        console.log(user)
-                        return (
-                            <div onClick={(event) => this.directToUrl(event, url)} class="user_button" style={{padding: '15px', margin: '0 auto', marginBottom: '1vh', cursor: 'pointer', height: 'fit-content'}}>
-                                <div style={{float: 'left', width: '50%'}}>
-                                    <p style={{lineHeight: '.5'}}>{this.state.matching_users[index]}</p>
-                                </div>
-                                <div style={{float: 'left', width: '50%'}}>
-                                    <p style={{lineHeight: '.5'}}>{new Date(user[0]).toLocaleString()}</p>
-                                </div>
-                                <p style={{clear: 'both', color: 'gray', overflowWrap: 'break-word', display: 'inline', verticalAlign: 'top', paddingTop: '0'}}>{user[1]}</p>
+        return (<div>
+            <div style={{width: '100%', justifyContent: 'center', display: 'flex', marginBottom: '2%'}}>
+                    <button hidden={!this.state.under_width} class="button4" style={{float: 'left', background: 'green', padding: '5px', marginRight: '8vw', marginTop: '3vh'}} onClick={(event) => this.changeView(event, true)}>Conversations</button>
+                    <button hidden={!this.state.under_width} class="button4" style={{float: 'left', background: 'green', padding: '5px', marginTop: '3vh'}} onClick={(event) => this.changeView(event, false)}>GolfTribe Courses</button>
+            </div>
+            <div hidden={this.state.under_width && !this.state.message_view} style={{width: width_form_a, float: 'left'}}>
+                <div style={{border: 'thick solid gray', minHeight: '80vh', borderRadius: '25px'}}>
+                <h3 style={{textAlign: 'center'}}>Message Conversations</h3>
+                {this.state.messages_list.map((user, index) => {
+                    var url = '/messages?id=' + this.state.matching_users[index];
+                    console.log(user)
+                    return (
+                        <div onClick={(event) => this.directToUrl(event, url)} class="user_button" style={{padding: '15px', margin: '0 auto', marginBottom: '1vh', cursor: 'pointer', height: 'fit-content', width: '90%'}}>
+                            <div style={{float: 'left', width: '50%'}}>
+                                <p style={{lineHeight: '.5'}}>{this.state.matching_users[index]}</p>
                             </div>
-                        );
-                        })}
-                        <div hidden={this.state.messages_list.length > 4}>
-                            <p style={{textAlign: 'center', fontWeight: 'bold'}}>You have no other text conversations</p>
+                            <div style={{float: 'left', width: '50%'}}>
+                                <p style={{lineHeight: '.5'}}>{new Date(user[0]).toLocaleString()}</p>
+                            </div>
+                            <p style={{clear: 'both', color: 'gray', overflowWrap: 'break-word', display: 'inline', verticalAlign: 'top', paddingTop: '0'}}>{user[1]}</p>
                         </div>
+                    );
+                    })}
+                    <div hidden={this.state.messages_list.length > 4}>
+                        <p style={{textAlign: 'center', fontWeight: 'bold'}}>You have no other text conversations</p>
                     </div>
                 </div>
-                <div hidden={this.state.under_width && this.state.message_view} style={{width: width_form_b, float: 'left', marginLeft: '3%', padding: '2%', border: 'thick solid black', borderRadius: '25px', minHeight: '65vh'}}>
-                    <CoursesOfferedComponent />
-                </div>
-            </div>)
-        }
+            </div>
+            <div hidden={this.state.under_width && this.state.message_view} style={{width: width_form_b, float: 'left', marginLeft: '3%', padding: '2%', border: 'thick solid black', borderRadius: '25px', minHeight: '65vh'}}>
+                <CoursesOfferedComponent />
+            </div>
+        </div>)
     }
 }
