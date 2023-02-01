1f0407043362295548376313829dd264d844e039
Thomas Kirby <tmkirby@umich.edu>
2023-01-31 23:28:17 -0500

    Change

--- src/components/TimesViewComponent.js
+++ src/components/TimesViewComponent.js
@@ -36,7 +36,7 @@ export class TimesViewComponent extends React.Component {
             friends_in_time: this.props.friends_in_time,
             search: "",
             user: this.props.user,
-            personalized: true,
+            personalized: this.props.personalized_user != null,
             personalized_user: this.props.personalized_user
         }
         if (this.state.friends_times === undefined) {
