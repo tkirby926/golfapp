c634c4982b4f0b7afd08ae5c4bb4a6413cba6b0e
Thomas Kirby <tmkirby@umich.edu>
2023-03-28 13:18:06 -0400

    C

--- src/components/PRFriendComponent.js
+++ src/components/PRFriendComponent.js
@@ -14,6 +14,9 @@ export class PRFriendComponent extends React.Component {
             return response.json();
         })
         .then((data) => {
+            if (data.error == "Y") {
+                throw(404);
+            }
             this.setState({ time_user_info: data.time_users, time_info: data.time_info});
         })
     }
@@ -30,7 +33,59 @@ export class PRFriendComponent extends React.Component {
         this.getTimeUsers();
     }
 
+    showUsers() {
+        if (this.state.tee_time_info[11].length > 0) {
+            var width = (100/this.state.tee_time_info[11].length).toString() + "%";
+            return (
+                <div style={{width: '100%', display: 'table'}}>
+                    <div>
+                        <h3>{this.state.gen_time_data[0]}</h3><br></br>
+                        <h3>{this.state.gen_time_data[1]}</h3>
+                    </div>
+                    <div style={{display: 'table-row', height: '40vh'}}>
+                        {this.state.tee_time_info[11].map((user, index) => { 
+                        console.log(user)
+                        var user_link = "/user/" + user[0];
+                        var src = user[9];
+                        if (src === null) {
+                            src = 'https://i.ibb.co/VBGR7B0/6d84a7006fbf.jpg';;
+                        }
+                        return (
+                            <form class="user_button" style={{width: {width}, display: 'table-cell', borderRadius: '50px'}}>
+                                <div>
+                                    <h4 style={{fontWeight: 'bold', textAlign: 'center', marginBottom:'0'}}>{user[0]}</h4>
+                                    <div style={{width: '100%', float: 'left', marginTop:'5%'}}>
+                                        <img src={src} style={{height: '50px', margin: '0 auto', borderRadius: '50%'}}></img>
+                                    </div>
+                                    
+                                </div>
+                                <div>
+                                
+                                    <div style={{color: '#080B3E'}}>Name: {user[1] + " " + user[2]}</div>
+                                    {this.checkNull(3, "Usual Score: ")}
+                                    {this.checkNull(4, "Favorite golf course played: ")}
+                                    {this.checkNull(5, "Drinking on the course: ")}
+                                    {this.checkNull(6, "Music on the course: ")}
+                                    {this.checkNull(7, "College/School: ")}
+                                    {this.checkNull(8, "Favorite Golfer: ")}
+                                    {this.checkNull(9, "Favorite Team: ")}
+                                    {this.checkNull(10, "Serious or casual golfer: ")}
+                                    {this.checkNull(11, "Wagering on the course: ")}
+                                    {this.checkNull(12, "Golf Cart or Walking: ")}
+                                    {this.checkNull(13, "Description: ")}
+                                </div>
+                            </form>
+                        )})}
+                    </div>
+                </div>)
+        }
+    }
+
     render() {
-        
+        return (
+            <div>
+                {this.showUsers()}
+            </div>
+        )
     }
 }
