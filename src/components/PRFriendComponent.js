45b8bc9edf09f0e0db307abbcce31aa9447fee02
Thomas Kirby <tmkirby@umich.edu>
2023-03-28 13:03:07 -0400

    C

--- src/components/PRFriendComponent.js
+++ src/components/PRFriendComponent.js
@@ -8,14 +8,24 @@ import UserProfile from './Userprofile';
 export class PRFriendComponent extends React.Component {
 
     getTimeUsers() {
-
+        fetch(UserProfile.getUrl() + "/api/v1/pr_users/" + this.state.user + "/" + this.state.timeid, { credentials: 'same-origin', method: 'GET' })
+        .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+        })
+        .then((data) => {
+            this.setState({ time_user_info: data.time_users, time_info: data.time_info});
+        })
     }
 
     constructor(props) {
         super(props);
+        const params = (new URL(document.location)).searchParams;
         this.state = {
             user: this.props.user,
-            time_user_info: []
+            time_user_info: [],
+            gen_time_data: [],
+            timeid: params.get('timeid')
         }
         this.getTimeUsers();
     }
