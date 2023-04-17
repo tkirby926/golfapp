88906d8ac51aaa41c6ac9160afcb7d9e88b7528e
Thomas Kirby <tmkirby@umich.edu>
2023-04-17 13:08:46 -0400

    Change

--- src/components/CourseLoginComponent.js
+++ src/components/CourseLoginComponent.js
@@ -20,26 +20,45 @@ export class CourseLoginComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            error_message: ""
+            error_message: "",
+            under_width: false
         }
     }
 
     render() {
+        this.state.under_width = false;
+        var form_wid = '50%';
+        var marg_lef = '25%';
+        if (window.innerWidth < 950) {
+            this.state.under_width = true;
+            form_wid = '90%';
+            marg_lef = '0%';
+        }
         return (
+        <div style={{minHeight: '100vh'}}>
+        <body>
         <div>
-        <div>
-            <div>
-                {this.state.error_message}
-            </div>
-            <form class="form" onSubmit={(event) => this.test_login(event)}>
-                Email: <input type="email" name="username"></input><br></br><br></br>
-                Password: <input type="password" name="password"></input><br></br><br></br>
-                <input type="submit" value="Submit"></input>
+            <button class="button4" style={{padding: '10px', marginLeft: '25vw', marginTop: '10vh', marginBottom: '10vh'}} onClick={(event) => this.goBack(event)}>Back</button>
+            <form class="form" style={{width: form_wid, marginLeft: marg_lef}} onSubmit={(event) => this.test_login(event)}>
+                <div hidden={this.state.too_many_attmpts}>
+                    <div style={{color: 'red'}}>
+                        {this.state.error_message}
+                    </div>
+                    Email: <input type="email" name="username"></input><br></br><br></br>
+                    Password: <input type="password" name="password"></input><br></br><br></br>
+                    <input type="submit" value="Submit"></input><br></br><br></br>
+                    <a href='/reset_pass'>Forgot Password</a>
+                </div>
+                <div hidden={!this.state.too_many_attmpts}>
+                    <p style={{textAlign: 'center'}}>You have failed your login too many times. An email has been sent to reset your password</p>
+                    <button class="button4" style={{width: '80%', marginLeft: '10%'}}>Resend email</button>
+                </div>
             </form>
+        </div>  
+        <div class='bottom_text'>
+            <p style={{fontSize: 'small'}}>To register your course with Golftribe, please email services@golftribe.com with your inquiry and our team will help you get started.</p>
         </div>
-        <div>
-            <a href="/register_course">Course not registered yet? Do it here</a><br></br>
-        </div>
+        </body>
         </div>
         )
     }
