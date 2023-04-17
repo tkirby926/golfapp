5e5f608b97998976c0a08319d789f1768f953f44
Thomas Kirby <tmkirby@umich.edu>
2023-04-17 11:07:20 -0400

    Change

--- src/components/MessagingComponent.js
+++ src/components/MessagingComponent.js
@@ -218,7 +218,7 @@ export class MessagingComponent extends React.Component {
                     <div style={{height: '22px'}}><h3 onClick={(event) => this.directToUrl(event, url)} style={{textAlign: 'center', cursor: 'pointer'}}>{this.state.message_receiver}</h3></div>
                     <div id="messagebox" onScroll={(event) => this.loadMore(event)} style={{height: '60vh', width: '90vw', maxWidth: '550px', position: 'relative', border: '5px solid #0E2F04', borderRadius: '25px', margin: 'auto', overflowY: 'auto', padding: '5px'}}>
                         <div style={{borderRadius: '25px', border: '5px green'}}>
-                            {this.state.messages.reverse().map((message, index) => {
+                            {this.state.messages.reverse().slice(0).map((message, index) => {
                                 if (message[1] === this.state.logged_username) {
                                     return this.showYourMessage(message[0])
                                 }
