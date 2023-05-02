abf375a7676375905db7213c882ce1b4b405d7c5
Thomas Kirby <tmkirby@umich.edu>
2023-05-02 00:27:05 -0400

    change

--- src/components/PaymentWindowContainer.js
+++ src/components/PaymentWindowContainer.js
@@ -51,16 +51,16 @@ export class PaymentContainerComponent extends React.Component {
         return (
         <div class="user_button_inv" style={{width: wid, margin: '0 auto', textAlign: 'center'}}>
             <div>
-                <button style={{marginTop: '5vh', width: '100px', marginBottom: '5vh', marginLeft: '5%', display: 'flex'}} onClick={(event) => this.returnToHome(event)} class="button4_inv">Cancel</button>
+                <button style={{marginTop: '5vh', width: '100px', marginBottom: '5vh', marginLeft: '5%', display: 'block', textAlign: 'center'}} onClick={(event) => this.returnToHome(event)} class="button4_inv">Cancel</button>
                 </div>
                 <h1>{this.state.time_info[0]}</h1><br></br>
                 <h2>Time: {date_readable}, {time_readable}</h2>
                 <h3>Number of Users Booking for:</h3>
-                <select style={{marginBottom: '1.5vh', marginLeft: '1vw'}} onChange={(event) => this.updateUsers(event)} name="users">
-                        <option value="1">1</option>
-                        <option hidden={this.state.time_info[11].length > 2}value="2">2</option>
-                        <option hidden={this.state.time_info[11].length > 1}value="3">3</option>
-                        <option hidden={this.state.time_info[11].length > 0} value="4">4</option></select>
+                <select style={{marginBottom: '1.5vh', marginLeft: '1vw', fontSize: '16px', fontFamily: 'Arial, Helvetica, sans-serif'}} onChange={(event) => this.updateUsers(event)} name="users">
+                        <option value="1">Just Me</option>
+                        <option hidden={this.state.time_info[11].length > 2}value="2">2 Total People</option>
+                        <option hidden={this.state.time_info[11].length > 1}value="3">3 Total People</option>
+                        <option hidden={this.state.time_info[11].length > 0} value="4">4 Total People</option></select>
                 <div style={{textAlign: 'center'}}>
                 <p style={{width: '90%', fontSize: 'small', margin: '0 auto'}}>Remember, you don't have to pay for your friends now. You will have the chance after booking to invite them to this teetime, where they will be sent an email link to join directly using their Golftribe account.</p>
             </div>
